449d17893b4e1295303e3d37bc40bb241ec85278
Mohamed Sayed <ms10596@gmail.com>
2019-04-06 19:33:41 +0200

    refactor: clean main

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,10 @@
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.regex.*;
-
 public class Main {
 
-    static ArrayList<Token> tokens = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         var sourceCode = Load.loadRaw();
-        var visitor = new Visitor(sourceCode.length());
-        var dictionary = Load.loadClasses();
-        for (var i : dictionary) {
-            var pattern = Pattern.compile((String) i.getValue());
-            var matcher = pattern.matcher(sourceCode);
-            while (matcher.find()) {
-                if (visitor.isVisited(matcher.start()) == false) {
-                    tokens.add(new Token((String) i.getKey(), matcher.group(), matcher.start()));
-                    visitor.visit(matcher.start(), matcher.end());
-                }
-            }
-        }
-        Collections.sort(tokens);
+        var tokens = Token.tokenize(sourceCode);
+        System.out.println(tokens);
         Save.saveTokens(tokens);
     }
 }
--- src/Token.java
+++ src/Token.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.regex.Pattern;
+
 public class Token implements Comparable<Token> {
     private String type, value;
     private int start;
@@ -16,5 +20,23 @@ public class Token implements Comparable<Token> {
     @Override
     public int compareTo(Token token) {
         return Integer.compare(this.start, token.start);
+    }
+
+    public static ArrayList<Token> tokenize(String sourceCode) throws Exception {
+        ArrayList<Token> tokens = new ArrayList<>();
+        var visitor = new Visitor(sourceCode.length());
+        var dictionary = Load.loadClasses();
+        for (var i : dictionary) {
+            var pattern = Pattern.compile((String) i.getValue());
+            var matcher = pattern.matcher(sourceCode);
+            while (matcher.find()) {
+                if (visitor.isVisited(matcher.start()) == false) {
+                    tokens.add(new Token((String) i.getKey(), matcher.group(), matcher.start()));
+                    visitor.visit(matcher.start(), matcher.end());
+                }
+            }
+        }
+        Collections.sort(tokens);
+        return tokens;
     }
 }
